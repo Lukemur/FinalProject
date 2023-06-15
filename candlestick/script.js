d21f6a89edcc557f7c06a811360e1f98b0e96d5e
Brandon Lee <124757679+bjlee97@users.noreply.github.com>
2023-06-14 17:45:05 -0700

    candlesitck

--- candlestick/script.js
+++ candlestick/script.js
@@ -1,46 +1,102 @@
+// Define the variable 'data'
+var data;
+var sma20Data;
+var sma50Data;
+
 // Load the JSON data
 fetch('json.txt')
-  .then(response => response.json())
-  .then(data => {
-    // Prepare the data for the candlestick graph
-    var series = [
-      {
-        data: data.map(item => ({
-          x: item.Date,
-          y: [
-            Math.round(item.Open),
-            Math.round(item.High),
-            Math.round(item.Low),
-            Math.round(item.Close),
-          ],
-        })),
-      },
-    ];
+    .then(response => response.json())
+    .then(jsonData => {
+        data = jsonData.data;
 
-    // Create the chart using ApexCharts
-    var options = {
-      series: series,
-      chart: {
-        type: 'candlestick',
-        height: 400,
-      },
-      title: {
-        text: 'Candlestick Chart',
-        align: 'center',
-      },
-      xaxis: {
-        type: 'datetime',
-      },
-      yaxis: {
-        labels: {
-          formatter: function (value) {
-            return Math.round(value);
-          },
-        },
-      },
-    };
+        // Prepare the data for the candlestick graph
+        var series = [
+            {
+                name: 'Candlestick',
+                data: data.map(item => ({
+                    x: new Date(item.Date),
+                    y: [
+                        Math.round(item.Open),
+                        Math.round(item.High),
+                        Math.round(item.Low),
+                        Math.round(item.Close),
+                    ],
+                })),
+            },
+            {
+                name: 'SMA 20',
+                data: sma20Data,
+            },
+            {
+                name: 'SMA 50',
+                data: sma50Data,
+            },
+        ];
 
-    var chart = new ApexCharts(document.querySelector('#chart'), options);
-    chart.render();
-  })
-  .catch(error => console.error(error));
+        // Create the chart using ApexCharts
+        var options = {
+            series: series,
+            // Rest of the options...
+        };
+
+        var chart = new ApexCharts(document.querySelector('#candlestick-chart'), options);
+        chart.render();
+
+        // Function to toggle SMA lines visibility
+        function toggleSMA() {
+            var sma20Series = chart.getSeriesByName('SMA 20');
+            var sma50Series = chart.getSeriesByName('SMA 50');
+
+            if (sma20Series.visible) {
+                chart.hideSeries('SMA 20');
+                chart.hideSeries('SMA 50');
+            } else {
+                chart.showSeries('SMA 20');
+                chart.showSeries('SMA 50');
+            }
+        }
+
+        // Add event listener to toggle SMA lines
+        document.getElementById('toggle-sma').addEventListener('click', toggleSMA);
+
+        // Load the SMA data
+        fetch('sma.json')
+            .then(response => response.json())
+            .then(smaData => {
+                sma20Data = smaData.data.map(item => ({
+                    x: new Date(item.Date),
+                    y: item.SMA_20,
+                }));
+
+                sma50Data = smaData.data.map(item => ({
+                    x: new Date(item.Date),
+                    y: item.SMA_50,
+                }));
+
+                // Update the series data with SMA data
+                chart.updateSeries([
+                    {
+                        name: 'Candlestick',
+                        data: data.map(item => ({
+                            x: new Date(item.Date),
+                            y: [
+                                Math.round(item.Open),
+                                Math.round(item.High),
+                                Math.round(item.Low),
+                                Math.round(item.Close),
+                            ],
+                        })),
+                    },
+                    {
+                        name: 'SMA 20',
+                        data: sma20Data,
+                    },
+                    {
+                        name: 'SMA 50',
+                        data: sma50Data,
+                    },
+                ]);
+            })
+            .catch(error => console.error(error));
+    })
+    .catch(error => console.error(error));
